1995f00312b575a359e62c55f1ac6009aa772bd8
Christopher-Tetteh-Nenebi <ctnenebi@aggies.ncat.edu>
2026-05-25 04:08:51 -0400

    Initial commit

---
+++ train.py
@@ -0,0 +1,29 @@
+# train.py
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+
+# Load dataset from a working source (kaggle/hosted)
+url = url = "https://raw.githubusercontent.com/plotly/datasets/master/diabetes.csv"
+df = pd.read_csv(url)
+
+print("Colums: ", df.columns.tolist())
+
+# Prepare data
+X = df[["Pregnancies", "Glucose", "BloodPressure", "BMI", "Age"]]
+y = df['Outcome']
+
+# Split 
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2,random_state=42)
+
+# Train model
+model = RandomForestClassifier()
+model.fit(X_train,y_train)
+
+# save model
+joblib.dump(model,'models/diabetes_model.pkl')
+print("Model saved as diabetes_model.pkl")
